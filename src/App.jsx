a6c5cafeb4eddd4980a3a7961164e672ce9ec074
Ferreira Josué <ferreira.josu@gmail.com>
2023-05-11 16:44:05 +0200

    grids and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Album from './components/Album'
+import NavBar from './components/NavBar'
+import data from './assets/data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Album />
+    <div>
+      <NavBar />
+      <div className="grid grid-cols-4 gap-4" style={{margin: '10px'}}>
+        {data.map((album) => (
+          <Album key={album.title_short} album={album}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import data from '../assets/data.js'
 
-const Album = props => {
+const Album = ({album}) => {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={data[0].album.cover} alt={data[0].title_short}/>
+            <img className="w-full" src={album.album.cover} alt={album.title_short}/>
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{data[0].title_short}</div>
-                <p className="text-gray-700 text-base">{data[0].title}</p>
+                <div className="font-bold text-xl mb-2">{album.title_short}</div>
+                <p className="text-gray-700 text-base">{album.title}</p>
             </div>
-            <video controls name="media" style={{padding: '10px', height: '10vh', width: '100%'}}><source src={data[0].preview} type="audio/mpeg"/></video>
+            <video controls name="media" style={{padding: '10px', height: '10vh', width: '100%'}}><source src={album.preview} type="audio/mpeg"/></video>
         </div>
     );
 };
 
 Album.propTypes = {
-    
+    album: PropTypes.shape({
+        title_short: PropTypes.string.isRequired,
+        title_short: PropTypes.string.isRequired,
+        preview: PropTypes.string.isRequired
+    }).isRequired
 };
 
 export default Album;
